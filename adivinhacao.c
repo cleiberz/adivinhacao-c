46f812b41f7a321d875a5f8d41c4d561ae4857dc
Cleiber Ziviani <cleiberz@yahoo.com.br>
2021-05-24 12:01:05 -0300

    commit inicial

---
+++ adivinhacao.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "portuguese");
+    //imprime o cabeçalho do nosso jogo.
+    printf("\n******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+    printf("******************************************\n");
+
+    int numerosecreto = 42;
+    int chute;
+    printf("Informe seu chute: ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d.\n", chute);
+    
+    int acertou = chute == numerosecreto;
+    int errouAcima = chute > numerosecreto;
+    int errouAbaixo = chute < numerosecreto;
+    
+    if(acertou){
+        printf("Parabéns! Você acertou.\n");
+        printf("Jogue de novo, você é um bom jogador.\n");
+    }
+    else {
+    	if (errouAbaixo){
+    		printf("Você errou! Seu chute foi menor que o número secreto.\n");
+		}
+		if (errouAcima){
+			printf("Você errou! Seu chute foi maior que o número secreto.\n");
+		}
+        
+        printf("Não desanime, tente de novo.\n");
+    }
+    
+    return (0);
+
+
+}
